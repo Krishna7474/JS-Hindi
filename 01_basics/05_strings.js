6e58b3a62b20b7c36569fbbf7264499707d4a747
Krishna Gaikwad <krishnagaikwad7474@gmail.com>
2026-02-17 08:29:37 +0000

    1

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,14 +5,36 @@ console.log(name + repoCount + " Value" );
 
 console.log(`Hello my is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String ('krishna')
+const gameName = new String ('krishna-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName._proto_);
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt('t'));
+console.log(gameName.charAt('2'));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne ="   Krishna     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://krishna.com%20Gaikwad"
+console.log(url.replace('%20','-'))
+
+console.log (url.includes(''))
+
+console.log(gameName.split('-'));
+
+
+
+
 
 
 
