95f1e57d2293ce075a834cbaa8bd42d32decd0a5
Krishna Gaikwad <krishnagaikwad7474@gmail.com>
2026-02-17 06:20:32 +0000

    1

---
+++ datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive 
+
+
+// 7 types : String , NUmber , Boolean ,null , undefined ,Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol ('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 545464646415646554n
+
+
+//Reference ( Non primitive)
+
+
+// Array , Objects ,Functions
+
+const heros = ["shaktimaan","naagraj","doga"];
+let myObj ={
+    name:"hitesh",
+    age : 22,
+
+} 
+const myFun = function(){
+    console.log("hello World");
+    
+}
+console.log(typeof bigNumber);
