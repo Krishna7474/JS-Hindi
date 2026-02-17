20e087d6abe61e7931c909d654501074ff12874d
Krishna Gaikwad <krishnagaikwad7474@gmail.com>
2026-02-17 05:13:39 +0000

    useful

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all Js code as newer version
+
+//alert(3 + 3)  // wea are using nodejs,not browser
+
+console.log (3+3);
+
+
+// code readability shoul be high
+console.log("Krishna")
+
+let name = "krishna"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => to pwer of 53
+// bigint =>
+// string => ""
+// boolean => true /false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log (typeof null); // object
+console.log(typeof undefined); // undefined
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "krishna"
+
+
+console.log(typeof score);
+console.log (typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;false => 0
+
+let someNumer  = 33
+
+let stringNumber= String(someNumer)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
