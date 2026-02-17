d07aa8bb2168d17a23e8433069ff27f1f765481b
Krishna Gaikwad <krishnagaikwad7474@gmail.com>
2026-02-17 05:33:04 +0000

    i

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,3 +19,55 @@ let stringNumber= String(someNumer)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+//**********************operations**************** */
+
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+
+console.log(2+2);
+console.log(2-2);
+
+console.log(2*2);
+console.log(2/3);
+console.log(2**3);
+console.log(2%3);
+
+let str1 = "helo"
+let str2 = "krishna"
+
+let str3 = str1 + str2
+console.log(str3);
+
+//console.log("1"+ 2);
+//console.log("1"+ 2 + 2);
+//console.log(1 + 2+ "2");
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study prefix and postfix on Mdn
+
+
+
+
+
+
+
+
+
+
+
+
+
