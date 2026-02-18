ee06da5e1d4d9e9f8d8a557c69dadbac05bec725
Krishna Gaikwad <krishnagaikwad7474@gmail.com>
2026-02-18 05:14:04 +0000

    2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,69 @@
+// const tinderUser = new Object()
+
+// const.tinderUser = {};
+
+// const tinderUser.id = "123abc"
+// // tinderUser.name = "Sammy"
+// // tinderUser.isLoggedIn = false
+
+// // console.log(tinderUser);
+
+// // const regularUser = {
+// //     email : "some@gmail.com"
+// //     fullname: {
+// //         userfullname: {
+// //             firstname: "krishna",
+// //             lastname: "gaikwad"
+// //         }
+// //     }
+// // }
+// // console.log(regularUser.fullname?.userfullname.firstname);
+
+// const obj1 = {1: "a",2: "b"}
+// const obj2 = {3: "a",4: "b"}
+// const obj4 = {5: "a",6: "b"}
+
+
+// // const obj3 = { obj1, obj2}
+// const obj3 = Object.assign({},obj1, obj2, obj4)
+
+// const obj4 = {...obj1,...obj2}
+// console.log(obj4);
+
+// const users = [
+//     {
+//         id: 1,
+//         email: "krishna@gmail.com"
+
+//     }
+// ]
+
+// users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys());
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "krishna"
+}
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     name: "krishna",
+//     coursename: "Js is hindi",
+//     price: "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
