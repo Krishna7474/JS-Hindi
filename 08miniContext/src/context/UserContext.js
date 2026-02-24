f4328eb918e44a4c5feb03ddb2b74231230aed07
Krishna Gaikwad <krishnagaikwad7474@gmail.com>
2026-02-24 06:56:21 +0000

    1

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import UserContext from './UserContext'
+import Login from '../components/Login';
+
+const UserContext = React.createContext()
+
+
+<UserContext>
+<Login  />  
+<Card>
+    <Data />
+</Card>     
+</UserContext>
+
+export default UserContext;
