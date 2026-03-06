7829081d70abfe6f33fcab6bc986f9592335711a
Krishna Gaikwad <krishnagaikwad7474@gmail.com>
2026-03-06 12:27:45 +0000

    1

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,9 +1,24 @@
 
 let index = 0 
 
-while (index <= 10)) {
+while (index <= 10) {
     console.log(`Value of index is ${index}`);
-    index = index + 2
-    
+    index = index + 2  
 }
 let myArray = ['flash',"batman","superman "]
+
+let arr = 0
+while (arr < arr.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+}while (score <= 10);
+
+
+
